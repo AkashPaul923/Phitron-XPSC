16b2e8e9ce44adaae3ad0d0ba9393b162c3a3ee3
AkashPaul923 <akashpaul923@gmail.com>
2025-08-05 15:08:23 +0600

    A. Two Elevators solved

---
+++ CodeForces-Problem/A_Two_Elevators.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        int x = a - 1;
+        int y;
+        if (b > c)
+            y = b - 1;
+        else
+            y = (c - b) + (c - 1);
+        if(x < y)
+            cout << "1\n";
+        else if(x > y)
+            cout << "2\n";
+        else
+            cout << "3\n";
+    }
+
+    return 0;
+}
