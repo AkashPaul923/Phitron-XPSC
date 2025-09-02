f178bb3dfae9633788d74189c45d7680e5ad0101
Akash Paul <akashpaul923@gmail.com>
2025-09-02 20:47:47 +0600

    three ducks solved

---
+++ CodeForces-Problem/A_Three_Decks.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if ((a + b + c) % 3 == 0 && b <= (a + b + c) / 3)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
