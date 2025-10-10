21c17a34fd0697d8beeb03969dad6f2a27f9abc2
Akash Paul <akashpaul923@gmail.com>
2025-10-10 21:54:30 +0600

    week 1 U, D  question solve

--- Newbie Solvers Club/Week-1/A_Mahmoud_and_Ehab_and_the_MEX.cpp
+++ Newbie Solvers Club/Week-1/A_Mahmoud_and_Ehab_and_the_MEX.cpp
@@ -5,31 +5,20 @@ int main()
 {
     int n, e;
     cin >> n >> e;
-    int op = 0;
     int a[n];
+    int fre[101] = {0};
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
-        if (a[i] == e)
-        {
-            op = 1;
-        }
+        fre[a[i]]++;
     }
-    int mx = -1;
-    for (int i = 0; i < n; i++)
-    {
-        if (i != a[i])
-            break;
-        mx++;
-    }
-    if (op == 1)
-    {
-        cout << e - mx + op;
-    }
-    else
-    {
-        cout << e - mx - 1;
-    }
+    int op = 0;
+    for (int i = 0; i < e; i++)
+        if (fre[i] == 0)
+            op++;
+    if (fre[e] > 0)
+        op++;
+    cout << op;
 
     return 0;
 }
---
+++ Newbie Solvers Club/Week-1/U_Feynman.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while (cin >> n)
+    {
+        if (n > 0)
+        {
+            cout << (n * (n + 1) * ((2 * n) + 1)) / 6 << endl;
+        }
+    }
+
+    return 0;
+}
